39a6c1110974b2d465468de97737f1e1c18c3210
RaziurC4S5E <105146023+RaziurC4S5E@users.noreply.github.com>
2025-10-18 13:14:24 +0600

    added a getHumanChoice() function in script.js

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ function getComputerChoice () {
     let value = Math.random();
     // console.log(value);
 
-    if (value < 0.33) return 'Rock';
-    else if(value < 0.66) return 'Paper';
-    else return 'Scissors';
+    if (value < 0.33) return 'rock';
+    else if(value < 0.66) return 'paper';
+    else return 'scissors';
 }
 
+function getHumanChoice() {
+    let value = prompt("Choose only one (Rock/Paper/Scissors): ");
+    return value.toLowerCase();
+}
+
+// let value = prompt("Choose only one (Rock/Paper/Scissors): ");
+    
+// lowerValue = value.toLowerCase();
+// if (!(lowerValue === 'rock') || !(lowerValue === 'paper') || !(lowerValue === 'scissors')) {
+//     console.log("Choose Again");
+//     value = prompt("Choose only one (Rock/Paper/Scissors: ");
+// }
+// console.log(value.toLowerCase());
